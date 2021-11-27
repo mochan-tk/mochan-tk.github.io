27920ab01f6165bede44f63d1d1e6f6150b4c0bb
mochan_tk <takashi1.kawamoto@gmail.com>
2021-11-27 11:50:28 +0000

    commit

--- docs/liff-starter/liff-starter.js
+++ docs/liff-starter/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1656508316-k7jNojXm";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1656670722-WbaG2XbP";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
